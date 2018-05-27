cad3ea15ecc54ae04dd4902d8f48730aef8db05e
Ernesto Herrera Arias <ernestoharias@gmail.com>
2018-05-27 13:02:22 -0400

    Updated two (2) YouTube trailers links. Code Style and Comments ok.

--- entertainment_center.py
+++ entertainment_center.py
@@ -4,16 +4,17 @@ import json
 
 
 def get_movies():
-  movies = []
-  json_movies = json.load(open('movies.txt'))
-  for json_movie in json_movies:
-    movies.append(media.Movie(json_movie['movie_title'],
-                              json_movie['movie_storyline'],
-                              json_movie['poster_image'],
-                              json_movie['trailer_youtube']
-                              )
-                  )
-  return movies
+    # Parse Json "movies.txt" file and return a media.Movie array.
+    movies = []
+    json_movies = json.load(open('movies.txt'))
+    for json_movie in json_movies:
+        movies.append(media.Movie(
+            json_movie['movie_title'],
+            json_movie['movie_storyline'],
+            json_movie['poster_image'],
+            json_movie['trailer_youtube'])
+        )
+    return movies
 
-
+# Create .html file with the movies array.
 fresh_tomatoes.open_movies_page(get_movies())
